44cd76aa9bea39cbe0924fcad2f35d63eea292b6
wxy <Ariavan168@Gamil.com>
2023-02-09 15:45:39 +0800

    添加了问题描述

--- parking.h
+++ parking.h
@@ -1,7 +1,20 @@
-#define PARK_SIZE 50000
+#define PARK_SIZE 5
 
 typedef struct
 {
 	Car space[PARK_SIZE];
 	int top;
-};
+}parking;
+
+void init(parking P)
+{
+	P->top = -1;
+}
+
+int pop(parking P,Car * temp)
+{
+	if(P->top == -1) return false;
+	
+	Car * temp = P.space[P.top--];
+	return true;
+}
